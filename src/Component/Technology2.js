3139a7d6dc244f91257d8966749c92653f250465
faboade oluwatosin <Faboadeoluwatosin@gmail.com>
2021-09-05 20:51:32 +0100

    email js and cloudinary img

--- src/Component/Technology2.js
+++ src/Component/Technology2.js
@@ -4,43 +4,43 @@ const Technology2 = () => {
     return (
         <div className="technology2">
             <div className="stack2">
-                    <img 
-                        src='http://assets.stickpng.com/thumbs/5847f5bdcef1014c0b5e489c.png'
-                        alt='html'
-                    />
-                    <img 
-                        src='https://cdn.freelogovectors.net/wp-content/uploads/2020/04/css-3-logo.png'
-                        alt='css3'
-                    />
-                    <img 
-                        src='https://www.freepnglogos.com/uploads/javascript-png/javascript-shield-logo-icon-2.png'
-                        alt='javascript'
-                    />
-                    <img 
-                        src='https://freepikpsd.com/media/2019/10/bootstrap-logo-png-1-Transparent-Images-Free.png'
-                        alt='Boostrap'
-                    />
-                    <img 
-                        src='https://dimitris.apeiro.gr/wp-content/uploads/2018/04/sass.png'
-                        alt='Sass'
-                    />
-                    {/* <img 
+                <img
+                    src='https://res.cloudinary.com/tosin/image/upload/v1630426690/5847f5bdcef1014c0b5e489c_rx8kvw.png'
+                    alt='html'
+                />
+                <img
+                    src='https://res.cloudinary.com/tosin/image/upload/v1630426848/portfolio/css-3-logo.png'
+                    alt='css3'
+                />
+                <img
+                    src='https://res.cloudinary.com/tosin/image/upload/v1630426933/portfolio/javascript-shield-logo-icon-2.png'
+                    alt='javascript'
+                />
+                <img
+                    src='https://freepikpsd.com/media/2019/10/bootstrap-logo-png-1-Transparent-Images-Free.png'
+                    alt='Boostrap'
+                />
+                <img
+                    src='https://dimitris.apeiro.gr/wp-content/uploads/2018/04/sass.png'
+                    alt='Sass'
+                />
+                {/* <img 
                         src='https://cdn-media-1.freecodecamp.org/images/1*FDNeKIUeUnf0XdqHmi7nsw.png'
                         alt='Material ui'
                     /> */}
-                    <img 
-                        src='https://storage.googleapis.com/blog-images-backup/1*3SVfBkNZI2f-sspiq59xcw.png'
-                        alt='react js'
-                    />
-                    <img 
-                        src='https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png'
-                        alt='Redux'
-                    />
-                    <img 
-                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png'
-                        alt='Git'
-                    />
-                
+                <img
+                    src='https://storage.googleapis.com/blog-images-backup/1*3SVfBkNZI2f-sspiq59xcw.png'
+                    alt='react js'
+                />
+                <img
+                    src='https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png'
+                    alt='Redux'
+                />
+                <img
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png'
+                    alt='Git'
+                />
+
             </div>
         </div>
     )
